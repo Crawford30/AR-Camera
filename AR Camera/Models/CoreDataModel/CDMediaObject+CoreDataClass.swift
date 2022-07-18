c8bcd8a3dcf593049ab23a4c653bc6c961c54b1a
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-18 17:00:42 +0300

    Created Entity and attributes and generated managed classes for the objects

---
+++ AR Camera/Models/CoreDataModel/CDMediaObject+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDMediaObject+CoreDataClass.swift
+//  AR Camera
+//
+//  Created by Joel Crawford on 7/18/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class CDMediaObject: NSManagedObject {
+
+}
---
+++ AR Camera/Models/CoreDataModel/CDMediaObject+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  CDMediaObject+CoreDataProperties.swift
+//  AR Camera
+//
+//  Created by Joel Crawford on 7/18/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CDMediaObject {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CDMediaObject> {
+        return NSFetchRequest<CDMediaObject>(entityName: "CDMediaObject")
+    }
+
+    @NSManaged public var id: String?
+    @NSManaged public var createDate: Date?
+    @NSManaged public var endDate: Date?
+    @NSManaged public var caption: String?
+    @NSManaged public var imageData: Data?
+    @NSManaged public var videoData: Data?
+
+}
+
+extension CDMediaObject : Identifiable {
+
+}
